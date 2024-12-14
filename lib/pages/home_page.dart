60c25cbf3aa15be9868b38ae0f3936ff1479566c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-14 12:40:23 -0500

    actualizacion!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,23 @@ class HomePage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
+                usersReference.get().then((value) {
+                  print(value);
+                  QuerySnapshot userCollection = value;
+                  List<QueryDocumentSnapshot> docs = userCollection.docs;
+                  print(docs);
+                  docs.forEach((element) {
+                    print(element.data());
+                  });
+                });
+              },
+              child: Text("Get data"),
+            ),
+            ElevatedButton(
+              onPressed: () {
                 usersReference
-                    .where("isPeruvian", isEqualTo: true)
+                    .where("name", isEqualTo: "Jhonny")
+                    .where("age", isGreaterThan: 10)
                     .get()
                     .then((value) {
                   print(value);
@@ -29,8 +44,43 @@ class HomePage extends StatelessWidget {
                   });
                 });
               },
-              child: Text("Get data"),
-            )
+              child: Text("Get data filtrada"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.add({
+                  "name": "Julio",
+                  "lastname": "Mar",
+                  "age": 35,
+                  "weigth": 80,
+                }).then((value) {
+                  print(value);
+                  print(value.id);
+                });
+              },
+              child: Text("Agregar usuario"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("id1").set(
+                  {
+                    "name": "Maria",
+                    "lastname": "Pacheco",
+                    "age": 58,
+                    "weigth": 75,
+                  },
+                );
+              },
+              child: Text("inserción con id"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("id1").update({
+                  "heigth": 1.78,
+                });
+              },
+              child: Text("Actualización"),
+            ),
           ],
         ),
       ),
