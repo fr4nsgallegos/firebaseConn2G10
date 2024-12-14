d199d85f74a9457b1bb57307122aeab94b147174
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-14 10:38:31 -0500

    firebase conn2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:firebaseconn2/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       home: HomePage(),
