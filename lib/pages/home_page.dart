b9374df319847e11732c1113e94a3dd8d9d3a7b3
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-14 12:16:06 -0500

    get with where

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  CollectionReference usersReference =
+      FirebaseFirestore.instance.collection("users");
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conexión a firestore"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                usersReference
+                    .where("isPeruvian", isEqualTo: true)
+                    .get()
+                    .then((value) {
+                  print(value);
+                  QuerySnapshot userCollection = value;
+                  List<QueryDocumentSnapshot> docs = userCollection.docs;
+                  print(docs);
+                  docs.forEach((element) {
+                    print(element.data());
+                  });
+                });
+              },
+              child: Text("Get data"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
